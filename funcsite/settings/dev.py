aad5a0e0aa2109e0375cd1a99d1dedab63758662
Melvyn Wright <melwright61@virginmedia.com>
2022-09-26 09:32:52 +0100

    initial commit

---
+++ funcsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-lv1=kbe3=uxjqw*(vxh0k78)alca@0qgri6#o!8a50v9q8)=r^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

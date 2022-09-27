ca3a78eb40b3eac889542d1a9700ecf5d451e572
Melvyn Wright <92119014+mmw111161@users.noreply.github.com>
2022-09-27 20:36:24 +0100

    Add files via upload
    Added django toolbars

--- funcsite/settings/dev.py
+++ funcsite/settings/dev.py
@@ -3,6 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-lv1=kbe3=uxjqw*(vxh0k78)alca@0qgri6#o!8a50v9q8)=r^"
 
@@ -11,6 +12,15 @@ ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+INSTALLED_APPS = INSTALLED_APPS + [
+    'debug_toolbar',
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ("127.0.0.1", "172.17.0.1")
 
 try:
     from .local import *
